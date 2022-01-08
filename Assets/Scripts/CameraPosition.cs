409ab2a12b06d39dd730aed73cbd3e3fbef0a60b
Mohamad Aly <mohammedelzaidy@gmail.com>
2022-01-08 20:39:52 +0100

    initial commit

---
+++ Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraPosition : MonoBehaviour
+{
+    private GameObject mainCamera;
+    private GameObject player;
+    public int cameraY = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.transform.position = player.transform.position + new Vector3(0, cameraY, 0);
+    }
+}
